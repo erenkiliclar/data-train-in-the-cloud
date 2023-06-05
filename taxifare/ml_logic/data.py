debe428d7a3e3442b4df53dcaeeb740dc7d7e9d0
Eren Kiliclar <erenkiliclar10@hotmail.com>
2023-06-05 16:51:58 +0200

    Challenge updated

--- taxifare/ml_logic/data.py
+++ taxifare/ml_logic/data.py
@@ -86,9 +86,19 @@ def load_data_to_bq(
 
     # 🎯 HINT for "*** TypeError: expected bytes, int found":
     # After preprocessing the data, your original column names are gone (print it to check),
-    # so ensure that your column names are *strings* that start with either 
+    # so ensure that your column names are *strings* that start with either
     # a *letter* or an *underscore*, as BQ does not accept anything else
 
-    pass  # YOUR CODE HERE
+    data.columns = [f"_{column}" if not str(column)[0].isalpha() and not str(column)[0] == "_" else str(column) for column in data.columns]
+
+    client = bigquery.Client()
+
+    write_mode = "WRITE_TRUNCATE" if truncate else "WRITE_APPEND"
+    job_config = bigquery.LoadJobConfig(write_disposition=write_mode)
+
+    print(f"\{'Write' if truncate else 'Append'} {full_table_name} ({data.shape[0]} rows)")
+
+    job = client.load_table_from_dataframe(data, full_table_name, job_config=job_config)
+    result = job.result()
 
     print(f"✅ Data saved to bigquery, with shape {data.shape}")
